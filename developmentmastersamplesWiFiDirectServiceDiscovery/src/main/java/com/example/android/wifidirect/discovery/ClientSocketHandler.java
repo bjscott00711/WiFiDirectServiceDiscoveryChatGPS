2a05d03db7dd1427b412d5a647158ddd30960480
scottbj <bjscott@hotmail.com>
2014-12-02 00:17:12 -0500

    Initial commit

---
+++ developmentmastersamplesWiFiDirectServiceDiscovery/src/main/java/com/example/android/wifidirect/discovery/ClientSocketHandler.java
@@ -0,0 +1,52 @@
+
+package com.example.android.wifidirect.discovery;
+
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ClientSocketHandler extends Thread {
+
+    private static final String TAG = "ClientSocketHandler";
+    private Handler handler;
+    private ChatManager chat;
+    private InetAddress mAddress;
+
+    public ClientSocketHandler(Handler handler, InetAddress groupOwnerAddress) {
+        Log.d(TAG, "ClientSocketHandler(Handler handler, InetAddress groupOwnerAddress)    (bjs)");    //bjs
+        this.handler = handler;
+        this.mAddress = groupOwnerAddress;
+    }
+
+    @Override
+    public void run() {
+        Log.d(TAG, "run()    (bjs)");    //bjs
+        Socket socket = new Socket();
+        try {
+            socket.bind(null);
+            socket.connect(new InetSocketAddress(mAddress.getHostAddress(),
+                    WiFiServiceDiscoveryActivity.SERVER_PORT), 5000);
+            Log.d(TAG, "Launching the I/O handler");
+            chat = new ChatManager(socket, handler);
+            new Thread(chat).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return;
+        }
+    }
+
+    public ChatManager getChat() {
+        Log.d(TAG, "getChat()    (bjs)");    //bjs
+        return chat;
+    }
+
+}
---
+++ developmentmastersamplesWiFiDirectServiceDiscovery/src/main/java/com/example/android/wifidirect/discovery/WiFiDirectServicesList.java
@@ -0,0 +1,117 @@
+
+package com.example.android.wifidirect.discovery;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple ListFragment that shows the available services as published by the
+ * peers
+ */
+public class WiFiDirectServicesList extends ListFragment {
+
+    WiFiDevicesAdapter listAdapter = null;
+    private static final String TAG = "WiFiDirectServicesList";   //bjs
+
+    interface DeviceClickListener {
+        public void connectP2p(WiFiP2pService wifiP2pService);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        Log.d(TAG,"onCreateView(LayoutInflater inflater, ViewGroup container,\n" +
+                "            Bundle savedInstanceState)    (bjs)");    //bjs
+        return inflater.inflate(R.layout.devices_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listAdapter = new WiFiDevicesAdapter(this.getActivity(),
+                android.R.layout.simple_list_item_2, android.R.id.text1,
+                new ArrayList<WiFiP2pService>());
+        setListAdapter(listAdapter);
+        Log.d(TAG,"onActivityCreated(Bundle savedInstanceState)    (bjs)");    //bjs
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        Log.d(TAG,"onListItemClick(ListView l, View v, int position, long id)    (bjs)");    //bjs
+        ((DeviceClickListener) getActivity()).connectP2p((WiFiP2pService) l
+                .getItemAtPosition(position));
+        ((TextView) v.findViewById(android.R.id.text2)).setText("Connecting");
+
+    }
+
+    public class WiFiDevicesAdapter extends ArrayAdapter<WiFiP2pService> {
+
+        private List<WiFiP2pService> items;
+
+        public WiFiDevicesAdapter(Context context, int resource,
+                int textViewResourceId, List<WiFiP2pService> items) {
+            super(context, resource, textViewResourceId, items);
+            this.items = items;
+            Log.d(TAG,"WiFiDevicesAdapter(Context context, int resource,\n" +
+                    "                int textViewResourceId, List<WiFiP2pService> items)    (bjs)");    //bjs
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d(TAG,"getView(int position, View convertView, ViewGroup parent)    (bjs)");    //bjs
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater) getActivity()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(android.R.layout.simple_list_item_2, null);
+            }
+            WiFiP2pService service = items.get(position);
+            if (service != null) {
+                TextView nameText = (TextView) v
+                        .findViewById(android.R.id.text1);
+
+                if (nameText != null) {
+                    nameText.setText(service.device.deviceName + " - " + service.instanceName);
+                }
+                TextView statusText = (TextView) v
+                        .findViewById(android.R.id.text2);
+                statusText.setText(getDeviceStatus(service.device.status));
+            }
+            return v;
+        }
+
+    }
+
+    public static String getDeviceStatus(int statusCode) {
+        Log.d(TAG,"getDeviceStatus(int statusCode)    (bjs)");    //bjs
+        switch (statusCode) {
+            case WifiP2pDevice.CONNECTED:
+                return "Connected";
+            case WifiP2pDevice.INVITED:
+                return "Invited";
+            case WifiP2pDevice.FAILED:
+                return "Failed";
+            case WifiP2pDevice.AVAILABLE:
+                return "Available";
+            case WifiP2pDevice.UNAVAILABLE:
+                return "Unavailable";
+            default:
+                return "Unknown";
+
+        }
+    }
+
+}
